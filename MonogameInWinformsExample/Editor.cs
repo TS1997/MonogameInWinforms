984db9d1f4a9be7d52c766d987685b1e90238137
TS1997 <tma.s@hotmail.com>
2016-06-16 12:48:48 +0200

    Adding files

---
+++ MonogameInWinformsExample/Editor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonogameInWinformsExample
+{
+    public class Editor : GraphicsDeviceControl
+    {
+        protected override void Draw()
+        {
+            GraphicsDevice.Clear(Color.Blue);
+        }
+
+        protected override void Initialize()
+        {
+            
+        }
+    }
+}
---
+++ MonogameInWinformsExample/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace MonogameInWinformsExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editor1 = new MonogameInWinformsExample.Editor();
+            this.SuspendLayout();
+            // 
+            // editor1
+            // 
+            this.editor1.Location = new System.Drawing.Point(13, 13);
+            this.editor1.Name = "editor1";
+            this.editor1.Size = new System.Drawing.Size(504, 405);
+            this.editor1.TabIndex = 0;
+            this.editor1.Text = "editor1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 430);
+            this.Controls.Add(this.editor1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Editor editor1;
+    }
+}
+
